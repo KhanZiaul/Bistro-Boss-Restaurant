2687cd109111722ae2a5d0d4e67cc55b1824fd63
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-23 22:05:14 +0600

    swipper images added

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -1,9 +1,9 @@
 
 const Title = ({subTitle,Title}) => {
     return (
-        <div className='text-center space-y-4 w-72'>
-            <p>{subTitle}</p>
-            <h2 className='border-t-2 border-b-2'>{Title}</h2> 
+        <div className='text-center space-y-4 w-72 mx-auto'>
+            <p className="text-yellow-500">{subTitle}</p>
+            <h2 className='border-t-2 border-b-2 p-4 text-4xl'>{Title}</h2> 
         </div>
     );
 };
--- src/Pages/Home/OrderOnline/OrderOnline.jsx
+++ src/Pages/Home/OrderOnline/OrderOnline.jsx
@@ -1,11 +1,49 @@
 import Title from '../../../Components/Title/Title';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/pagination";
+import "swiper/css";
+import { Pagination } from "swiper";
+
+import image1 from '../../../assets/home/slide1.jpg'
+import image2 from '../../../assets/home/slide2.jpg'
+import image3 from '../../../assets/home/slide3.jpg'
+import image4 from '../../../assets/home/slide4.jpg'
+import image5 from '../../../assets/home/slide5.jpg'
 
 const OrderOnline = () => {
     return (
         <div className='my-12'>
-            <Title 
-            subTitle={'---From 11:00am to 10:00pm---'} 
-            Title={'ORDER ONLINE'}></Title>
+            <Title
+                subTitle={'---From 11:00am to 10:00pm---'}
+                Title={'ORDER ONLINE'}></Title>
+            <div>
+                <Swiper
+                    slidesPerView={3}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper mb-16"
+                >
+                    <SwiperSlide>
+                        <img src={image1} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={image2} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={image3} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={image4} alt="" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={image5} alt="" />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+
         </div>
     );
 };
