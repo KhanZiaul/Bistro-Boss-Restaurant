e186498a890caadb639d5ecb5d4d2ed06190aba4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-23 20:16:16 +0600

    Nav done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Nav from '../Pages/Shared/Nav/Nav';
 
 const MainLayout = () => {
     return (
         <div>
+            <Nav></Nav>
             <Outlet></Outlet>
         </div>
     );
