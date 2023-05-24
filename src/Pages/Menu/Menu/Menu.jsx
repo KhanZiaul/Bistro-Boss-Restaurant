4d46db13e654e5591823e48c8a25836e287845f4
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-25 01:11:43 +0600

    dessert done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,17 +1,23 @@
 import Cover from '../Shared/Cover/Cover';
 import image1 from '../../../assets/menu/banner3.jpg'
+import image2 from '../../../assets/menu/dessert-bg.jpeg'
 import useMenuData from '../../../Hooks/useMenuData/useMenuData';
 import OrderFood from '../Shared/OrderFood.jsx/OrderFood';
 
 const Menu = () => {
 
     const [allMenu] = useMenuData()
+
     const offered = allMenu?.filter(popular => popular.category === 'offered')
+    const dessert = allMenu?.filter(popular => popular.category === 'dessert')
 
     return (
         <div>
             <Cover image={image1} title='OUR MENU' subTitle='WOULD YOU LIKE TO TRY A DISH ?'></Cover>
             <OrderFood title={"TODAY'S OFFER"} items={offered} subTitle={"--- Don't miss ---"}></OrderFood>
+
+            <Cover image={image2} title='dessert' subTitle="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+            <OrderFood items={dessert}></OrderFood>
         </div>
     );
 };
--- src/Pages/Menu/Shared/Cover/Cover.jsx
+++ src/Pages/Menu/Shared/Cover/Cover.jsx
@@ -7,8 +7,8 @@ const Cover = ({ image, title, subTitle }) => {
                 <div className="hero-overlay bg-opacity-60"></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="bg-black opacity-60 w-[350px] lg:w-[900px] h-[200px] lg:h-[250px] flex flex-col justify-center items-center">
-                        <div className="opacity-100">
-                            <h1 className="mb-5 text-3xl lg:text-5xl opacity-100 font-bold">{title}</h1>
+                        <div className="opacity-100 p-5">
+                            <h1 className="mb-5 text-3xl lg:text-5xl opacity-100 font-bold uppercase">{title}</h1>
                             <p className="mb-5">{subTitle}</p>
                         </div>
                     </div>
