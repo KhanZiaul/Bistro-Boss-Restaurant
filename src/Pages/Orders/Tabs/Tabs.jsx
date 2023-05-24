03f2405c2bb6ec124ca55e529fa55105b73b15fe
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-25 03:16:49 +0600

    added tab card

--- src/Pages/Orders/Tabs/Tabs.jsx
+++ src/Pages/Orders/Tabs/Tabs.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
-const Tabs = () => {
+
+const Tabs = ({item}) => {
+
+    const {image , name , recipe} = item
+
     return (
         <div>
-            
+            <div className="card w-full lg:w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={image} alt="item" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{name}</h2>
+                    <p>{recipe}</p>
+                    <div className="card-actions">
+                        <Link><button className="btn btn-outline bottom-0 border-b-4 block mx-auto">ADD TO CART</button></Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
