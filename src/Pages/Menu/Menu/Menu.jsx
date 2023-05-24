adee61c52f6a8a56c9de0d7566a822c75cf52bc2
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-25 01:15:56 +0600

    pizza done

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -10,14 +10,29 @@ const Menu = () => {
 
     const offered = allMenu?.filter(popular => popular.category === 'offered')
     const dessert = allMenu?.filter(popular => popular.category === 'dessert')
+    const pizza = allMenu?.filter(popular => popular.category === 'pizza')
 
     return (
         <div>
+
+            {/* Offered */}
+
             <Cover image={image1} title='OUR MENU' subTitle='WOULD YOU LIKE TO TRY A DISH ?'></Cover>
+
             <OrderFood title={"TODAY'S OFFER"} items={offered} subTitle={"--- Don't miss ---"}></OrderFood>
 
+            {/* Dessert */}
+
             <Cover image={image2} title='dessert' subTitle="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+
             <OrderFood items={dessert}></OrderFood>
+
+            {/* Pizza */}
+
+            <Cover image={image2} title='pizza' subTitle="Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."></Cover>
+
+            <OrderFood items={pizza}></OrderFood>
+
         </div>
     );
 };
