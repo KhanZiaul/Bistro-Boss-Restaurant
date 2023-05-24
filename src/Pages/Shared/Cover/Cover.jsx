7c561a0054ee3a47aca320bd8d87c2dfc2b71b8d
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-25 03:02:56 +0600

    react parallax added

--- src/Pages/Shared/Cover/Cover.jsx
+++ src/Pages/Shared/Cover/Cover.jsx
@@ -1,17 +1,28 @@
+import { Parallax} from 'react-parallax';
+
 const Cover = ({ image, title, subTitle }) => {
     return (
         <div>
-            <div className="hero h-[600px]" style={{ backgroundImage: `url(${image})` }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="bg-black bg-opacity-60 w-[350px] lg:w-[900px] h-[200px] lg:h-[250px] flex flex-col justify-center items-center">
-                        <div className="p-5">
-                            <h1 className="mb-5 text-3xl lg:text-5xl font-bold uppercase">{title}</h1>
-                            <p className="mb-5">{subTitle}</p>
+            <Parallax
+                blur={{ min: -15, max: 15 }}
+                bgImage={image}
+                bgImageAlt="the dog"
+                strength={-200}
+            >
+
+                <div className="hero h-[600px]">
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-center text-neutral-content">
+                        <div className="bg-black bg-opacity-60 w-[350px] lg:w-[900px] h-[200px] lg:h-[250px] flex flex-col justify-center items-center">
+                            <div className="p-5">
+                                <h1 className="mb-5 text-3xl lg:text-5xl font-bold uppercase">{title}</h1>
+                                <p className="mb-5">{subTitle}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+
+            </Parallax>
         </div>
     );
 };
