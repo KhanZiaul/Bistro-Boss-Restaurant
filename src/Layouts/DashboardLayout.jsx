086390c1eb01e4384f973ff3f00d92ff4ac8efed
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-05-28 20:57:42 +0600

    DashboardLayout added

---
+++ src/Layouts/DashboardLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet } from "react-router-dom";
+
+const DashboardLayout = () => {
+    return (
+        <div className="drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content">
+                <!-- Page content here -->
+                <label htmlFor="my-drawer" className="btn btn-primary drawer-button">Open drawer</label>
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer" className="drawer-overlay"></label>
+                <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+                    <Outlet></Outlet>
+                    <li><a>Sidebar Item 1</a></li>
+                    <li><a>My Cart</a></li>
+
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
