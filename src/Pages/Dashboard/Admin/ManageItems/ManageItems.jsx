7dcb754871894d9fb8902c2dfc0bdf491465a998
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-02 04:04:11 +0600

    Delete item handler done

--- src/Pages/Dashboard/Admin/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/Admin/ManageItems/ManageItems.jsx
@@ -2,10 +2,40 @@ import { BsTrash } from "react-icons/bs";
 import Title from "../../../../Components/Title/Title";
 import { BiEdit } from "react-icons/bi";
 import useMenuData from "../../../../Hooks/useMenuData/useMenuData";
+import axios from "axios";
+import useAxiosSrcure from "../../../../Hooks/useAxiosSecure/useAxiosSecure";
+import Swal from "sweetalert2";
 
 
 const ManageItems = () => {
     const [allMenu, refetch] = useMenuData()
+    const [axiosSecure] = useAxiosSrcure()
+
+    function handleDelete(id) {
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/menu/${id}`)
+                    .then(res => {
+                        refetch()
+                        console.log(res.data)
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        )
+                    })
+            }
+        })
+    }
 
     return (
         <div>
@@ -33,7 +63,7 @@ const ManageItems = () => {
                         </thead>
                         <tbody>
                             {
-                                allMenu?.map((menu , index) => {
+                                allMenu?.map((menu, index) => {
                                     return (
                                         <>
                                             <tr key={menu._id}>
@@ -59,7 +89,7 @@ const ManageItems = () => {
                                                     <BiEdit className="text-white h-8 w-8 bg-[#D1A054] hover:bg-[#df8f17] cursor-pointer p-2 rounded-md"></BiEdit>
                                                 </th>
                                                 <th>
-                                                    <button className="btn btn-ghost btn-xs btn-lg"><BsTrash className="text-red-600"></BsTrash> </button>
+                                                    <button onClick={() => handleDelete(menu._id)} className="btn btn-ghost btn-xs btn-lg"><BsTrash className="text-red-600"></BsTrash> </button>
                                                 </th>
                                             </tr>
                                         </>
