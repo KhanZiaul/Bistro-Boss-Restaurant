52426e620d139557f387c5a7215d9ffb6a88ff30
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-06-04 19:02:20 +0600

    Bar Chart added

--- src/Pages/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/Pages/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -5,13 +5,14 @@ import { useContext } from 'react';
 import { AuthContext } from '../../../../Provider/AuthProvider/AuthProvider';
 import useAxiosSrcure from '../../../../Hooks/useAxiosSecure/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 
 const AdminHome = () => {
 
     const { user, loading } = useContext(AuthContext)
     const [axiosSecure] = useAxiosSrcure()
 
-    const { data: stats = {}, isLoading } = useQuery({
+    const { data: stats = {} } = useQuery({
         queryKey: ['adminStats'],
         enabled: !loading,
         queryFn: async () => {
@@ -19,7 +20,35 @@ const AdminHome = () => {
             return res.data
         }
     })
-    console.log(stats)
+
+    const { data: chartData = [] } = useQuery({
+        queryKey: ['chartData'],
+        enabled: !loading,
+        queryFn: async () => {
+            const res = await axiosSecure('/adminOrder')
+            return res.data
+        }
+    })
+
+    console.log(chartData)
+
+    // Bar Chart ----------------------------------
+
+    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+
+    const getPath = (x, y, width, height) => {
+        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+        ${x + width / 2}, ${y}
+        C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+        Z`;
+    };
+
+    const TriangleBar = (props) => {
+        const { fill, x, y, width, height } = props;
+
+        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+    };
+
     return (
         <div>
             <h2 className='text-2xl'>Welcome Back !! {user?.displayName}</h2>
@@ -58,6 +87,35 @@ const AdminHome = () => {
                     <div className="text-2xl font-semibold">Orders</div>
                     <div className="text-2xl font-semibold">{stats?.orders}
                     </div>
+                </div>
+
+            </div>
+
+            <div className='flex gap-5 mt-10'>
+
+                <div className='w-1/2'>
+
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={chartData}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="category" />
+                        <YAxis />
+                        <Bar dataKey="total" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+                            {chartData?.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+                            ))}
+                        </Bar>
+                    </BarChart>
+
                 </div>
 
             </div>
